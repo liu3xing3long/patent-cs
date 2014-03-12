17010e76ba1ad72e624798c6238a0a49c14f855f
liu3xing3long <liu3xing3long@163.com>
2014-03-12 23:20:15 +0800

    外观&&实用

--- patentCS/Form3.Designer.cs
+++ patentCS/Form3.Designer.cs
@@ -30,35 +30,48 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(59, 71);
+            this.button1.Location = new System.Drawing.Point(59, 22);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(155, 70);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Statistics_1";
+            this.button1.Text = "发明统计";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.Location = new System.Drawing.Point(59, 169);
+            this.button2.Location = new System.Drawing.Point(59, 104);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(155, 70);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Statistics_2";
+            this.button2.Text = "实用新型统计";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(59, 183);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 70);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "外观设计统计";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form3";
@@ -72,5 +85,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- patentCS/Form3.cs
+++ patentCS/Form3.cs
@@ -63,7 +63,7 @@ namespace patentCS
         private void button1_Click(object sender, EventArgs e)
         {
             int nCMPS = vCompanies.Count;
-            FileStream fsFilestatistics = new FileStream("申请统计.txt", FileMode.Create);
+            FileStream fsFilestatistics = new FileStream("发明统计.txt", FileMode.Create);
             StreamWriter srFileBat = new StreamWriter(fsFilestatistics);
 
             for (int i = 0; i < nCMPS; i++)
@@ -277,7 +277,286 @@ namespace patentCS
 
         private void button2_Click(object sender, EventArgs e)
         {
+            int nCMPS = vCompanies.Count;
+            FileStream fsFilestatistics = new FileStream("实用统计.txt", FileMode.Create);
+            StreamWriter srFileBat = new StreamWriter(fsFilestatistics);
 
+            for (int i = 0; i < nCMPS; i++)
+            {
+                string sComName = vCompanies[i];
+                string sFileName = sComName + "实用新型.txt";
+
+                Console.WriteLine(">>>>>>>>>>>> Collecting " + sComName + " Begin >>>>>>>>>>>");
+                srFileBat.WriteLine("@" + sComName);
+                int nCount09 = 0;
+                int nCount10 = 0;
+                int nCount11 = 0;
+                int nCount12 = 0;
+                int nCount13 = 0;
+                int nCountV09 = 0;
+                int nCountV10 = 0;
+                int nCountV11 = 0;
+                int nCountV12 = 0;
+                int nCountV13 = 0; 
+                try
+                {
+                    FileStream fsFileBat = new FileStream(sFileName, FileMode.Open);
+                    StreamReader swFileBat = new StreamReader(fsFileBat);
+
+                    Dictionary<string, List<CPatenInfo>> lTmpPatentInfo = new Dictionary<string, List<CPatenInfo>>();
+                    while (!swFileBat.EndOfStream)
+                    {
+                        string sShenqinghao = "";
+                        string sGongkaiMing = "";
+                        string sIPC = "";
+                        string sShenqingri = "";
+                        string sGongkairi = "";
+                        // read 11 lines
+                        for (int j = 0; j < 10; j++)
+                        {
+                            string strTmp = swFileBat.ReadLine();
+                            strTmp = strTmp.Trim();
+                            // 申请号
+                            if (j == 0)
+                            {
+                                int indexS = strTmp.IndexOf(":");
+                                sShenqinghao = strTmp.Substring(indexS + 1);
+                            }
+                            else if (j == 1)
+                            {
+                                int indexS = strTmp.IndexOf(":");
+                                sShenqingri = strTmp.Substring(indexS + 1);
+                            }
+                            else if (j == 3)
+                            {
+                                int indexS = strTmp.IndexOf(":");
+                                sGongkairi = strTmp.Substring(indexS + 1);
+                            }
+                            else if (j == 4)//公开名称
+                            {
+                                int indexS = strTmp.IndexOf(":");
+                                sGongkaiMing = strTmp.Substring(indexS + 1);
+                            }
+                            else if (j == 5)
+                            {
+                                int indexS = strTmp.IndexOf(":");
+                                int indexE = strTmp.IndexOf(";");
+                                if (indexE == -1)
+                                {
+                                    sIPC = strTmp.Substring(indexS + 1);
+                                }
+                                else
+                                {
+                                    sIPC = strTmp.Substring(indexS + 1, indexE - indexS - 1);
+                                }
+                            }
+                        }
+                        CPatenInfo patentInfo = new CPatenInfo();
+                        patentInfo.sShenqingri = sShenqingri;
+                        patentInfo.sMingcheng = sGongkaiMing;
+                        patentInfo.sShenqinghao = sShenqinghao;
+                        patentInfo.sIPC = sIPC;
+                        patentInfo.sGongkairi = sGongkairi;
+
+                        int indexNian = sShenqingri.IndexOf(".");
+                        string sShenqingNian = sShenqingri.Substring(0, indexNian);
+                        switch (sShenqingNian)
+                        {
+                            case "2009":
+                                nCount09++;
+                                break;
+                            case "2010":
+                                nCount10++;
+                                break;
+                            case "2011":
+                                nCount11++;
+                                break;
+                            case "2012":
+                                nCount12++;
+                                break;
+                            case "2013":
+                                nCount13++;
+                                break;
+                        }
+
+                        indexNian = sGongkairi.IndexOf(".");
+                        sShenqingNian = sGongkairi.Substring(0, indexNian);
+                        switch (sShenqingNian)
+                        {
+                            case "2009":
+                                nCountV09++;
+                                break;
+                            case "2010":
+                                nCountV10++;
+                                break;
+                            case "2011":
+                                nCountV11++;
+                                break;
+                            case "2012":
+                                nCountV12++;
+                                break;
+                            case "2013":
+                                nCountV13++;
+                                break;
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine("===========ERROR " + ex.Message + "================");
+                }
+                srFileBat.WriteLine("09 " + nCount09 + "\t" + nCountV09);
+                srFileBat.WriteLine("10 " + nCount10 + "\t" + nCountV10);
+                srFileBat.WriteLine("11 " + nCount11 + "\t" + nCountV11);
+                srFileBat.WriteLine("12 " + nCount12 + "\t" + nCountV12);
+                srFileBat.WriteLine("13 " + nCount13 + "\t" + nCountV13);
+                srFileBat.WriteLine("#" + sComName);
+            }
+
+            srFileBat.Close();
+            fsFilestatistics.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int nCMPS = vCompanies.Count;
+            FileStream fsFilestatistics = new FileStream("外观设计统计.txt", FileMode.Create);
+            StreamWriter srFileBat = new StreamWriter(fsFilestatistics);
+
+            for (int i = 0; i < nCMPS; i++)
+            {
+                string sComName = vCompanies[i];
+                string sFileName = sComName + "外观设计.txt";
+
+                Console.WriteLine(">>>>>>>>>>>> Collecting " + sComName + " Begin >>>>>>>>>>>");
+                srFileBat.WriteLine("@" + sComName);
+                int nCount09 = 0;
+                int nCount10 = 0;
+                int nCount11 = 0;
+                int nCount12 = 0;
+                int nCount13 = 0;
+                int nCountV09 = 0;
+                int nCountV10 = 0;
+                int nCountV11 = 0;
+                int nCountV12 = 0;
+                int nCountV13 = 0;
+                try
+                {
+                    FileStream fsFileBat = new FileStream(sFileName, FileMode.Open);
+                    StreamReader swFileBat = new StreamReader(fsFileBat);
+
+                    Dictionary<string, List<CPatenInfo>> lTmpPatentInfo = new Dictionary<string, List<CPatenInfo>>();
+                    while (!swFileBat.EndOfStream)
+                    {
+                        string sShenqinghao = "";
+                        string sGongkaiMing = "";
+                        string sIPC = "";
+                        string sShenqingri = "";
+                        string sGongkairi = "";
+                        // read 11 lines
+                        for (int j = 0; j < 10; j++)
+                        {
+                            string strTmp = swFileBat.ReadLine();
+                            strTmp = strTmp.Trim();
+                            // 申请号
+                            if (j == 0)
+                            {
+                                int indexS = strTmp.IndexOf(":");
+                                sShenqinghao = strTmp.Substring(indexS + 1);
+                            }
+                            else if (j == 1)
+                            {
+                                int indexS = strTmp.IndexOf(":");
+                                sShenqingri = strTmp.Substring(indexS + 1);
+                            }
+                            else if (j == 3)
+                            {
+                                int indexS = strTmp.IndexOf(":");
+                                sGongkairi = strTmp.Substring(indexS + 1);
+                            }
+                            else if (j == 4)//公开名称
+                            {
+                                int indexS = strTmp.IndexOf(":");
+                                sGongkaiMing = strTmp.Substring(indexS + 1);
+                            }
+                            else if (j == 5)
+                            {
+                                int indexS = strTmp.IndexOf(":");
+                                int indexE = strTmp.IndexOf(";");
+                                if (indexE == -1)
+                                {
+                                    sIPC = strTmp.Substring(indexS + 1);
+                                }
+                                else
+                                {
+                                    sIPC = strTmp.Substring(indexS + 1, indexE - indexS - 1);
+                                }
+                            }
+                        }
+                        CPatenInfo patentInfo = new CPatenInfo();
+                        patentInfo.sShenqingri = sShenqingri;
+                        patentInfo.sMingcheng = sGongkaiMing;
+                        patentInfo.sShenqinghao = sShenqinghao;
+                        patentInfo.sIPC = sIPC;
+                        patentInfo.sGongkairi = sGongkairi;
+
+                        int indexNian = sShenqingri.IndexOf(".");
+                        string sShenqingNian = sShenqingri.Substring(0, indexNian);
+                        switch (sShenqingNian)
+                        {
+                            case "2009":
+                                nCount09++;
+                                break;
+                            case "2010":
+                                nCount10++;
+                                break;
+                            case "2011":
+                                nCount11++;
+                                break;
+                            case "2012":
+                                nCount12++;
+                                break;
+                            case "2013":
+                                nCount13++;
+                                break;
+                        }
+
+                        indexNian = sGongkairi.IndexOf(".");
+                        sShenqingNian = sGongkairi.Substring(0, indexNian);
+                        switch (sShenqingNian)
+                        {
+                            case "2009":
+                                nCountV09++;
+                                break;
+                            case "2010":
+                                nCountV10++;
+                                break;
+                            case "2011":
+                                nCountV11++;
+                                break;
+                            case "2012":
+                                nCountV12++;
+                                break;
+                            case "2013":
+                                nCountV13++;
+                                break;
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine("===========ERROR " + ex.Message + "================");
+                }
+                srFileBat.WriteLine("09 " + nCount09 + "\t" + nCountV09);
+                srFileBat.WriteLine("10 " + nCount10 + "\t" + nCountV10);
+                srFileBat.WriteLine("11 " + nCount11 + "\t" + nCountV11);
+                srFileBat.WriteLine("12 " + nCount12 + "\t" + nCountV12);
+                srFileBat.WriteLine("13 " + nCount13 + "\t" + nCountV13);
+                srFileBat.WriteLine("#" + sComName);
+            }
+
+            srFileBat.Close();
+            fsFilestatistics.Close();
         }
     }
 }
